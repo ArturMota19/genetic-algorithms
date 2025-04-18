ccfe5e47d4dd4a3c81877ccc553e8a75dd9b7234
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-04-18 10:08:31 -0300

    Add initial population

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 """
 Desenvolver uma solução baseada em algoritmos genéticos 
 para maximizar o valor total de itens inseridos em uma 
@@ -18,7 +20,29 @@ items = [ #id, peso e valor
     {"id": "I", "weight": 3.5, "value": 60},
     {"id": "J", "weight": 2.5, "value": 55}
 ]
-# o objetivo é seguir os seguintes passos:
-# nova população -> popupulação inicial -> seleção -> crossover -> mutação -> nova população...
-# o cromossomo vai ser representado como um vetor de 0s e 1s, seguindo a ordem dos itens de cima
-# [0, 1, 1...] -> item B e C, e assim por diante
+"""
+===============================================================================================
+o objetivo é seguir os seguintes passos:
+nova população -> popupulação inicial -> seleção -> crossover -> mutação -> nova população...
+o cromossomo vai ser representado como um vetor de 0s e 1s, seguindo a ordem dos itens de cima
+[0, 1, 1...] -> item B e C, e assim por diante
+FUNÇÃO OBJETIVO: 
+Maximizar o valor, sujeito ao peso máximo de 15 kg
+===============================================================================================
+"""
+# Passo 1 - Criar a população inicial, tamanho 50 individuos
+def generate_initial_population(population_size, num_items):
+  population = [] # Define a popul. como vazia
+  for _ in range(population_size): # Faço um for pra preencher, a depender do tamanho que passei lá embaixo
+    cromossome = [random.randint(0, 1) for _ in range(num_items)] # for pra gerar cromossomos
+    population.append(cromossome) # adiciono o cromossomo na população
+  return population
+
+# Gerar a população inicial
+size = 50 #definido no enunciado da questao
+num_items = len(items) # tenho que pegar o "n", qtd de itens do problema
+population = generate_initial_population(size, num_items)
+# PRINT DE TESTES
+for individual in population:
+    print(individual)
+
