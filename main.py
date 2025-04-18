bce6cc7271a01b2ea3b1e610e6d1f908eea61287
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-04-18 09:52:48 -0300

    Add initial params

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Desenvolver uma solução baseada em algoritmos genéticos 
+para maximizar o valor total de itens inseridos em uma 
+mochila com limite de peso de 15 kg, utilizando seleção 
+por roleta e representações binárias.
+"""
+
+limitWeight = 15  # peso maximo
+items = [
+    {"id": "A", "weight": 2, "value": 40},
+    {"id": "B", "weight": 3, "value": 50},
+    {"id": "C", "weight": 4, "value": 65},
+    {"id": "D", "weight": 5, "value": 80},
+    {"id": "E", "weight": 7, "value": 110},
+    {"id": "F", "weight": 1, "value": 15},
+    {"id": "G", "weight": 6, "value": 90},
+    {"id": "H", "weight": 4.5, "value": 70},
+    {"id": "I", "weight": 3.5, "value": 60},
+    {"id": "J", "weight": 2.5, "value": 55}
+]
